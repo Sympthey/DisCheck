cb72cf563ce77f649fdfc459aad760ca40598832
Tyrao <66280234+Tyrao@users.noreply.github.com>
2020-10-15 20:45:16 +0100

    Version 1
    Completed the checker.

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# Async Discord Token Checker
- A little script to see if a discord token is working or not using aiohttp
+# Asynchronous Discord Token Checker
+> Account Also known as 'Token' checker for the popular discord.com service
+
+---
+
+## Table Of Contents
+- [Installation](#Installation) 
+- [How To Use](#How-To-Use)
+- [Developer Info](#Developer-Contact)
+- [License](#License)
+
+---
+## Installation
+- Download Python 3 - python.org
+- Download the contents of the github repo.
+- Install the modules needed in the requirements.txt `pip install requirements.txt`
+---
+## How To Use
+After you have completed the [installtion](#Installation) its as easy as running the script.<br> This can be done like so `python main.py`. Once it has ran a dialog box will show, naviagte to where your discord tokens are saved and then it will run the checker.<br>
+`The Results are saved into the 'results' folder.`
+
+---
+## License
+MIT License
+
+Copyright (c) [2020] [Sympthey]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+[Back To The Top](#Asynchronous-Discord-Token-Checker)
+
+---
+# Developer Contact
+
+- Discord: Sympthey#9640
+
+[Back To The Top](#Asynchronous-Discord-Token-Checker)
---
+++ source/main.py
@@ -0,0 +1,71 @@
+import aiohttp, asyncio, aiofiles, os, easygui, sys
+from colorama import Fore, init
+
+# Without this coloramam does not always display colours correcrly.
+init()
+
+# Our Function to read the tokens in the token file passed.
+async def read_tokens(token_file):
+    async with aiofiles.open(token_file) as opened_token_file:
+        tokens = await opened_token_file.readlines()
+        return tokens
+
+async def write_tokens(token, verified):
+    path = os.getcwd()
+    if verified == "Valid":
+        async with aiofiles.open(f"{path}/results/Valid Tokens.txt", mode="a") as verified_tokens:
+            await verified_tokens.write(token+"\n")
+
+    if verified == "Invalid":
+        async with aiofiles.open(f"{path}/results/Invalid Tokens.txt", mode="a") as invalid_tokens:
+            await invalid_tokens.write(token+"\n")
+    
+    if verified == "Locked":
+        async with aiofiles.open(f"{path}/results/Locked Tokens.txt", mode="a") as locked_tokens:
+            await locked_tokens.write(token+"\n")
+
+async def check_token(token):
+    async with aiohttp.ClientSession() as hit_endpoint:
+        discord_api_endpoint = "https://discordapp.com/api/v6/users/@me/guilds"
+        discord_headers = {
+            "Content-Type": "application/json",
+            "authorization": str(token)
+        }
+        send_data = await hit_endpoint.get(discord_api_endpoint, headers=discord_headers)
+        if send_data.status == 200:
+            check_json = await send_data.json()
+            print(Fore.GREEN+f"[VALID] - {token}")
+            await write_tokens(token, "Valid")
+
+        if send_data.status == 403:
+            print(Fore.YELLOW+f"[LOCKED] - {token}")
+            await write_tokens(token, "Locked")
+
+        if send_data.status == 401:
+            print(Fore.RED+f"[INVALID] - {token}{token}")
+            await write_tokens(token, "Invalid")
+
+        if send_data.status == 429:
+            print(Fore.WHITE+f"[WARNING] IP ADDRESS RATE LIMITED!")
+        else:
+            # If any other condition happens (should not)
+            pass
+
+async def main():
+    path = os.getcwd()
+    open(f"{path}/results/Valid Tokens.txt", "w").close()
+    open(f"{path}/results/Invalid Tokens.txt", "w").close()
+    open(f"{path}/results/Locked Tokens.txt", "w").close()
+    sys.stdout.write(f'\x1b]2;[DisCheck - Discord Token Checker] | Developer Sympthey\x07')
+    print(Fore.WHITE+"CHOOSE FILE WITH DISCORD TOKENS")
+    file_path = easygui.fileopenbox()
+    tokens = await read_tokens(file_path)
+    amount_tokens = str(len(tokens))
+    sys.stdout.write(f'\x1b]2;[DisCheck - Discord Token Checker] | Developer Sympthey | Tokens Loaded: {amount_tokens}\x07')
+    await asyncio.sleep(1.2)
+    for token in tokens:
+        cleaned_token = str(token).strip()
+        await check_token(cleaned_token)
+    
+
+asyncio.run(main())
